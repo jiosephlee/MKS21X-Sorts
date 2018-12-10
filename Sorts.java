a161ee24ea5ecba2b619022711640bb056ea974e
John Dogit config --global user.name John <jlee01@stuy.edu>
2018-12-10 14:51:55 -0500

    worked in class

--- Sorts.java
+++ Sorts.java
@@ -1,9 +1,14 @@
-public class sorts{
+public class Sorts{
   public static void main(String[] args) {
     int[] data = {2,1,3,7,0,-1,4,2,7};
     selectionSort(data);
     for (int x = 0; x < data.length; x++){
       System.out.print(data[x] + " ");
+    }
+    int[] data3 = {2,1,1,1,5,5,5,5,1,3,7,0,13,13,-5,5,-1,4,2,7};
+    selectionSort(data3);
+    for (int x = 0; x < data3.length; x++){
+      System.out.print(data3[x] + " ");
     }
     System.out.println();
     int[] data2 = {2,1,3,7,0,-1,4,2,7};
@@ -43,6 +48,23 @@ public class sorts{
           swaps++;
         }
       }
+    }
+  }
+
+  public static void insertionSort(int[] data){
+    int min = 0;
+    int hold = 0;
+    int holdind = 0;
+    for (int x = 1; x < data.length; x++){
+      for (int y = x; y > -1 && data[x] < data[x-1] ; y--){
+        if {
+          min = data[y];
+          holdind = y;
+        }
+      }
+      hold = data[x];
+      data[x] = min;
+      data[holdind] = hold;
     }
   }
 
